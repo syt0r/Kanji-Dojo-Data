9483a229c81f859ac3abb71e35a052fcbbefc249
syt0r <shuliak.yaroslav@gmail.com>
2025-03-01 01:39:46 +0200

    Add supported vocab decks csv

--- src/main/kotlin/ProjectData.kt
+++ src/main/kotlin/ProjectData.kt
@@ -15,6 +15,7 @@ object ProjectData {
     val exportDir = File("data")
     val exportCharactersDir = File(exportDir, "characters")
     val exportExpressionsDir = File(exportDir, "expressions")
+    val supportedVocab = File(exportDir, "supported_vocab.csv")
     val exportLetterDecksDir = File(exportDir, "letter_decks")
     val exportVocabDecksDir = File(exportDir, "vocab_decks")
 
